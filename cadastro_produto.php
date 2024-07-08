2842b2f495dc510aa59c1e53f582d1918ed3b0a1
marshfellow42 <123888466+marshfellow42@users.noreply.github.com>
2024-07-08 17:16:57 -0300

    Mudar o nome do website

--- cadastro_produto.php
+++ cadastro_produto.php
@@ -13,7 +13,7 @@
     <div class="px-md-3">
         <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">Ponto do Poder</a>
+              <a class="navbar-brand" href="#">Metacon</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
